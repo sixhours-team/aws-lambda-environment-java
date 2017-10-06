a7fa41dd2732355335a04a70d12e770be42f40b4
Igor Bolic <igor.bolic@sixhours.io>
2017-10-06 14:00:33 +0200

    Remove lombok due to missing JDK 9 support

--- src/main/java/io/sixhours/conf/Configuration.java
+++ src/main/java/io/sixhours/conf/Configuration.java
@@ -1,22 +1,51 @@
 package io.sixhours.conf;
 
 import io.sixhours.env.Environment;
-import lombok.Data;
 
 /**
  * Configuration properties.
  *
  * @author Igor Bolic
  */
-@Data
 public class Configuration {
 
     private Environment environment;
     private User user;
 
-    @Data
+    public Environment getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
     public static final class User {
         private String firstName;
         private String lastName;
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
     }
 }
